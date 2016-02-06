15e886bde5cf70623d21a2d28957159919e3738c
CptJamesCook <mrniceguyjames@gmail.com>
2016-02-05 19:42:31 -0800

    bgbkhkjhk

--- server.py
+++ server.py
@@ -1,18 +1,17 @@
-from graph import  UndirectedGraph, Graph
+from graph import  WeightedGraph, Graph
 import math
 
 #calculates the distance between two points
 def cost_distance(u,v):
     dist = math.sqrt((u[0]-v[0])**2+(u[1]-v[1])**2)
-    return dist
+    return int(dist)
 
 # Reads and creates a graph from a file
 def read_city_graph(filename):
     #set the three variables we are gonna output
-    EdGraph = UndirectedGraph()
+    EdGraph = WeightedGraph()
     roadNames = []
     locations = {}
-    weights = {}
     """
     Goes through each line of the file
     and creates a vertice or an edge
@@ -26,24 +25,31 @@ def read_city_graph(filename):
                 #appends the information we need about the vertice
                 locations[int(line[1])] = [int(float(line[2])*100000),int(float(line[3])*100000)]
             elif line[0] == "E":
-                EdGraph.add_edge(int(line[1]),int(line[2]))
-                roadNames.append([int(line[1]),int(line[2]), line[3].strip()])
-                weights[(int(line[1]),int(line[2]))] = cost_distance(locations[int(line[1])],locations[int(line[2])])
-    return(EdGraph,roadNames,locations,weights)
+                EdGraph.add_edge(int(line[1]), int(line[2]), cost_distance(locations[int(line[1])],
+                                                                           locations[int(line[2])]))
+                roadNames.append([int(line[1]), int(line[2]), line[3].strip()])
+
+    return EdGraph, roadNames, locations
+
 #the following functions gets the closest vertices to the start and end of the given input
 def closestVertices(locations, startx, starty, endx, endy):
-    startLoc = [startx,starty]
+    startLoc = [startx, starty]
     endLoc = [endx, endy]
-    startVert = 0
-    endVert = 0
+    pstartVert = {}
+    pendVert = {}
     for x in locations:
-        pstartVert = cost_distance(locations[x], startLoc)
-        pendVert = cost_distance(locations[x], endLoc)
-
+        print(x)
+        pstartVert[x] = cost_distance(locations[x], startLoc)
+        print(pstartVert)
+        pendVert[x] = cost_distance(locations[x], endLoc)
+        print(pendVert)
+    #sets the smallest value associated with a key to the start and end vertice
+    startVert = min(pstartVert, key = pstartVert.get)
+    endVert = min(pendVert, key = pendVert.get)
+    return startVert, endVert
 #The variable association below is purely for readability
-v = read_city_graph('edmonton-roads-2.0.1.txt')
+v = read_city_graph('testGraph')
 EdGraph = v[0]
 roadNames = v[1]
 locations = v[2]
-weights = v[3]
-print(weights)
+print(closestVertices(locations, 300000,200000,400000,400000))
